cf144b97ce34f1f5ee171de4ee1789330e34d7fc
ebi <ebid.py@gmail.com>
2025-01-21 15:49:00 +0330

    data insertion into mongodb

--- push_data.py
+++ push_data.py
@@ -12,17 +12,45 @@ MONGO_DB_URL = os.getenv("MONGO_DB_URL")
 import certifi
 ca=certifi.where()
 
-import pabdas as pd
+import pandas as pd
 import numpy as np
+from networksecurity.logging.logger import logging
+from networksecurity.exception.exception import NetworkSecurityException
 
-uri = MONGO_DB_URL
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    def csv_to_json_convertor(self, file_path):
+        try:
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    def insert_data_mongodb(self,records,database,collection):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+            self.mongo_client=MongoClient(MONGO_DB_URL)
+            self.database=self.mongo_client[self.database]
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return (len(self.records))
+        except Exception as e:
+            raise NetworkDataExtract(e,sys)
+
+if __name__ == '__main__':
+    FILE_PATH="Network_Data/phisingData.csv"
+    DATABASE="networkSecurity"
+    collection="NetworkData"
+    networkobj=NetworkDataExtract()
+    records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
+    no_of_records=networkobj.insert_data_mongodb(records,DATABASE,collection)
+    print(no_of_records)
