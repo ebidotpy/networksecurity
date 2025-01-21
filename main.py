d3ad27c85a1348f4bc8746bf9d2cb485c7d74add
ebi <ebid.py@gmail.com>
2025-01-21 19:41:08 +0330

    dataingestion implementation

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+from networksecurity.components.data_ingestion import DataIngestion
+
+
+
 from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig
+
 import sys
-from networksecurity.logging import logger
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     try:
-        logger.logging.info("Enter the try block")
-        a=1/0
-        print("This will not be printed",a)
+        trainingpipelineconfig=TrainingPipelineConfig()
+        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion=DataIngestion(dataingestionconfig)
+        logging.info("Initiate the data ingestion")
+        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
+        print(dataingestionartifact)
     except Exception as e:
-           raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e,sys)
+
+
+
+
+
